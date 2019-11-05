b430c10d5e0a48c42a88906604f9d3f7f0abace0
Isabel An <isabel.an@wustl.edu>
2019-11-05 12:20:31 -0600

    finished
    finished product

--- script.js
+++ script.js
@@ -1,53 +1,20 @@
-//document.getElementById('one').addEventListener("click",thisSlide(0));
-//document.getElementById('one').addEventListener("click",open);
-//document.getElementById('two').addEventListener("click",thisSlide(1));
-//document.getElementById('two').addEventListener("click",open);
+var photoIndex=-1;
 
-//document.getElementById('three').addEventListener("click",thisSlide(2));
-//document.getElementById('three').addEventListener("click",open);
+var photoArray = [document.getElementById('one'), document.getElementById('two'),
+document.getElementById('three'),document.getElementById('four'),
+document.getElementById('five'),document.getElementById('six'),
+document.getElementById('seven'),document.getElementById('eight'),
+document.getElementById('nine'),document.getElementById('ten'),
+document.getElementById('eleven'),document.getElementById('twelve')];
 
-//document.getElementById('four').addEventListener("click",thisSlide(3));
-//document.getElementById('four').addEventListener("click",open);
-
-//document.getElementById('five').addEventListener("click",thisSlide(4));
-//document.getElementById('five').addEventListener("click",open);
-
-//document.getElementById('six').addEventListener("click",thisSlide(5));
-//document.getElementById('six').addEventListener("click",open);
-
-//document.getElementById('seven').addEventListener("click",thisSlide(6));
-//document.getElementById('seven').addEventListener("click",open);
-
-//document.getElementById('eight').addEventListener("click",thisSlide(7));
-//document.getElementById('eight').addEventListener("click",open);
-//document.getElementById('nine').addEventListener("click",open);
-//document.getElementById('ten').addEventListener("click",open);
-//document.getElementById('eleven').addEventListener("click",open);
-//document.getElementById('twelve').addEventListener("click",open);
-
-
-var photoIndex=(-1);
-
-var photoArray =
-[document.getElementById("one"),
-document.getElementById("two"),
-document.getElementById("three"),
-document.getElementById('four'),
-document.getElementById('five'),
-document.getElementById('six'),
-document.getElementById('seven'),
-document.getElementById('eight'),
-document.getElementById('nine'),
-document.getElementById('ten'),
-document.getElementById('eleven'),
-document.getElementById('twelve')];
+document.getElementById('close-btn').addEventListener('click',close);
+document.getElementById('prev-btn').addEventListener('click',prev);
+document.getElementById('next-btn').addEventListener('click',next);
 
 for (i=0;i<photoArray.length;i++){
-  photoArray[i].addEventListener("click",open);
+  photoArray[i].addEventListener('click',open);
 }
-document.getElementById('close-btn').addEventListener("click",close);
-document.getElementById('prev-btn').addEventListener("click",prev);
-document.getElementById('next-btn').addEventListener("click",next);
+
 
 function open(){
   if (photoIndex=-1){
@@ -63,11 +30,22 @@ function close(){
   document.getElementById('zoom').style.display="none";
 }
 
+function next(){
+  if(photoIndex>=11){
+    document.getElementById('photo-big').src=photoArray[photoIndex].src;
+  }
+  else{
+  photoIndex=photoIndex-(-1);
+  document.getElementById('photo-big').src=photoArray[photoIndex].src;
+  }
+}
+
 function prev(){
+  if(photoIndex<=0){
+    document.getElementById('photo-big').src=photoArray[photoIndex].src;
+  }
+  else{
   photoIndex-=1;
   document.getElementById('photo-big').src=photoArray[photoIndex].src;
-}
-function next(){
-  photoIndex+=1;
-  document.getElementById('photo-big').src=photoArray[photoIndex].src;
+  }
 }
